e6d24b71912813438f4d5b1fd964ea5cb96d7b2c
James Hanlon <mail@jameswhanlon.com>
2023-07-03 14:26:25 +0100

    Webpack JS elements

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: './theme/static/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'output/theme/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+};
