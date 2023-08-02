adb92ba9499c991596ca413a72de13ded3dcea5d
James Hanlon <mail@jameswhanlon.com>
2023-08-02 14:53:54 +0100

    Revert "Add draft note on silicon infrastructure (#18)"
    This reverts commit 79454470a75cca3dd445aa7e6bd0ebc41109e7d7.

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,23 +5,22 @@ import sys
 import os
 import logging
 sys.path.insert(0, os.getcwd())
-from thumbnail import get_thumbnail, get_image
+from thumbnail import get_thumbnail
 
 AUTHOR = u'James W. Hanlon'
 SITENAME = u'James W. Hanlon'
 SITEURL = 'http://jameswhanlon.com'
 
-REMOTE_PREFIX = 'https://jwh.ams3.digitaloceanspaces.com/homepage'
+ASSETS_PREFIX = 'https://jwh.ams3.digitaloceanspaces.com/homepage'
 
 def get_asset_url(filepath):
-    return REMOTE_PREFIX+'/'+filepath
+    return ASSETS_PREFIX+'/'+filepath
 
 def foo(blah):
     return blah
 
 JINJA_FILTERS = { 'asset': get_asset_url,
-                  'thumbnail': get_thumbnail,
-                  'image': get_image, }
+                  'thumbnail': get_thumbnail, }
 
 PATH = 'content'
 IMAGE_PATH = 'images'
