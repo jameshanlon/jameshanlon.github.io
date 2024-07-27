7e609c4fc7fdd206998fa4d2fc7cd81bbd895f96
James Hanlon <mail@jameswhanlon.com>
2024-07-27 20:25:29 +0000

    Add dark mode and a toggle button

--- theme/static/js/index.js
+++ theme/static/js/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'lightbox2';
 import 'lightbox2/dist/css/lightbox.min.css';
